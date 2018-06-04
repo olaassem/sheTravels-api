5c4feee5de379bc48ae0b4e88d1c4ab9d04c6b85
Ola Assem <o.assem@gmail.com>
2018-06-04 02:47:25 +0200

    successful google maps, place details, & photo reference api

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-//G O O G L E    P L A C E S    C O D E//
+
 
 function initialize(){
    initAutocomplete();
@@ -26,17 +26,16 @@ function codeAddress(address){
 	}, function( results, status ){
 		//if valid query input
 		if (status === 'OK') {
-			console.log(results);
-
 			let queryLatitude = results[0].geometry.location.lat();
 			let queryLongitude = results[0].geometry.location.lng();
       let placeid = results[0].place_id;
-      console.log(placeid);
 
-      getDetails(placeid);
+      //Call api functions and pass lat and lng params through them
+			initMap( queryLatitude, queryLongitude );
+      getDetails( placeid );
       	//if invalid query input
     	} else {
-    		console.log(status);
+    		console.log( status );
     	}
   	});
 }
@@ -47,14 +46,36 @@ function fillInAddress(){
 }
 
 function getDetails(placeid){
-  console.log(`this is the ${placeid}!`);
   $.getJSON( (`https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeid}&key=AIzaSyA6ECb06GHjgfRQjrOJKy6tQqScBimbFmA`))
 	.done(function ( data ){
-		console.log( data )
-	}).fail(function ( data ){
-		alert('getdetials function Ajax Call Failed!')
+		console.log( data );
+    let photoRef = data.result.photos[0].photo_reference;
+    console.log(photoRef);
+    getImg( photoRef );
+
+    $('.locationInfo').html(`
+      <p class="name">${data.result.name}</p>
+      <p class="address">${data.result.formatted_address}</p>
+      `)
+
+      // <p class="country">${data.result.address_components[3].long_name}</p>
+
+
+	}).fail(function ( status ){
+		alert('getDetails function Ajax Call Failed!')
 	});
 }
+
+
+function getImg(photoRef){
+  console.log(photoRef);
+
+    $('.locationImg').html(`
+      <img src="https://maps.googleapis.com/maps/api/place/photo?maxwidth=300&photoreference=${photoRef}&key=AIzaSyA6ECb06GHjgfRQjrOJKy6tQqScBimbFmA" alt="" />
+      `)
+}
+
+
 
 
 
@@ -63,16 +84,18 @@ function getDetails(placeid){
 //  Google Maps Marker Icons
 //const GOOGLE_MAPS_ENDPNT = 'https://maps.googleapis.com/maps/api/js';
 //const GOOGLE_MAPS_API_KEY = 'AIzaSyA6ECb06GHjgfRQjrOJKy6tQqScBimbFmA';
-
+//
 //Create map
-function initMap( lat, lng, index ){
-	let location ={
+function initMap( lat, lng ){
+	let location = {
 		lat: parseFloat(lat),
 		lng: parseFloat(lng)
 	};
 
+  console.log(location);
+
 	//New map
-	let map = new google.maps.Map(document.getElementById('map' + index),{
+	let map = new google.maps.Map(document.getElementById('map'),{
 		zoom: 15,
 		center: location
 	});
@@ -82,4 +105,12 @@ function initMap( lat, lng, index ){
 		map: map,
 		animation: google.maps.Animation.DROP
 	});
+}
+
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
 }
